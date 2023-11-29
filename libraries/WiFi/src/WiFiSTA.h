e7ffc2e51e7b3e1b3e9c2eda33fa829e15174331
Frederic Henrichs <frederic@tinkerforge.com>
2023-11-29 10:28:25 +0100

    arduino_wifi: patch wifi begin for wpa enterprise.
     - add certificate sizes
     - actually use channel, bssid and connect arguments

--- libraries/WiFi/src/WiFiSTA.h
+++ libraries/WiFi/src/WiFiSTA.h
@@ -44,7 +44,7 @@ class WiFiSTAClass
 
 public:
 
-    wl_status_t begin(const char* wpa2_ssid, wpa2_auth_method_t method, const char* wpa2_identity=NULL, const char* wpa2_username=NULL, const char *wpa2_password=NULL, const char* ca_pem=NULL, const char* client_crt=NULL, const char* client_key=NULL, int32_t channel=0, const uint8_t* bssid=0, bool connect=true);
+    wl_status_t begin(const char* wpa2_ssid, wpa2_auth_method_t method, const char* wpa2_identity=NULL, const char* wpa2_username=NULL, const char *wpa2_password=NULL, const char* ca_pem=NULL, const size_t ca_size = 0, const char* client_crt=NULL, const size_t client_crt_size = 0, const char* client_key=NULL, const size_t client_key_size = 0, const char *client_key_password = NULL, int32_t channel=0, const uint8_t* bssid=0, bool connect=true);
     wl_status_t begin(const String& wpa2_ssid, wpa2_auth_method_t method, const String& wpa2_identity = (const char*)NULL, const String& wpa2_username = (const char*)NULL, const String& wpa2_password = (const char*)NULL, const String& ca_pem = (const char*)NULL, const String& client_crt = (const char*)NULL, const String& client_key = (const char*)NULL, int32_t channel=0, const uint8_t* bssid=0, bool connect=true) {
         return begin(wpa2_ssid.c_str(), method, wpa2_identity.c_str(), wpa2_username.c_str(), wpa2_password.c_str(), ca_pem.c_str(), client_crt.c_str(), client_key.c_str(), channel, bssid, connect);
     }
@@ -89,7 +89,7 @@ public:
     IPAddress broadcastIP();
     IPAddress networkID();
     uint8_t subnetCIDR();
-    
+
     bool enableIpV6();
     IPv6Address localIPv6();
 
@@ -104,7 +104,7 @@ public:
     int8_t RSSI();
 
     static void _setStatus(wl_status_t status);
-    
+
 protected:
     static bool _useStaticIp;
     static bool _autoReconnect;
@@ -112,7 +112,7 @@ protected:
     static wifi_scan_method_t _scanMethod;
     static wifi_sort_method_t _sortMethod;
 
-public: 
+public:
     bool beginSmartConfig(smartconfig_type_t type = SC_TYPE_ESPTOUCH, char* crypt_key = NULL);
     bool stopSmartConfig();
     bool smartConfigDone();
